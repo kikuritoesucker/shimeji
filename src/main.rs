f0a5a6403d0c37706c0869c43aa174d935c576c3
kikurihiroi <14887544+kikurihiroi@user.noreply.gitee.com>
2024-10-16 23:46:10 +0800

    20241016

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod linalg;
 mod tween;
+mod io;
+mod event;
 
 use linalg::*;
+
 fn main() {
     let p = cmp_from!(1.0, 1.0);
     let q = cmp_from!(1.0, 0.5);
-
-    println!("{:?}", p.inverse() * q * p);
 }
